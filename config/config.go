c6077701d387eb6b5b5962e9273e2f0476e83d5e
satrio wibowo <wibowosatrio2002@gmail.com>
2023-03-20 17:23:55 +0700

    add: menambahkan config

--- config/config.go
+++ config/config.go
@@ -1,1 +1,28 @@
 package config
+
+import (
+	"log"
+
+	"github.com/spf13/viper"
+)
+
+var (
+	DBname     string
+	DbPort     string
+	DbDriver   string
+	DbPassword string
+)
+
+func init() {
+	viper.SetConfigType("json")
+	viper.SetConfigName("config")
+	viper.AddConfigPath(".")
+	err := viper.ReadInConfig()
+	if err != nil {
+		log.Fatalf("Error Membaca File, %s", err.Error())
+	}
+	DBname = viper.GetString("db.name")
+	DbPort = viper.GetString("db.port")
+	DbDriver = viper.GetString("db.driver")
+	DbPassword = viper.GetString("db.password")
+}
--- db/db.go
+++ db/db.go
@@ -1,1 +1,21 @@
 package db
+
+import (
+	"database/sql"
+	"fmt"
+	"time"
+
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/wanta-zulfikri/Projek_unit1/config"
+	"github.com/wanta-zulfikri/Projek_unit1/helper"
+)
+
+func InitDb() *sql.DB {
+	db, err := sql.Open(config.DbDriver, fmt.Sprintf("root:%s@tcp(localhost:%s)/%s?parseTime=True", config.DbPassword, config.DbPort, config.DBname))
+	helper.PanicIfError(err)
+	db.SetMaxOpenConns(20)
+	db.SetMaxIdleConns(5)
+	db.SetConnMaxLifetime(60 * time.Minute)
+	db.SetConnMaxIdleTime(10 * time.Minute)
+	return db
+}
